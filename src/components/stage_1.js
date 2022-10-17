cf1790b67f17d309e716101f00125462085e9850
xander <karimi.xander@gmail.com>
2022-10-17 10:36:47 -0700

    validate

--- src/components/stage_1.js
+++ src/components/stage_1.js
@@ -1,5 +1,58 @@
+import React, { useRef, useContext, useState } from "react";
+import { Button, Form, Alert } from "react-bootstrap"
+
+import { MyContext } from "../context";
+
 function Stage1() {
-  return <div>stage 1</div>;
+  const textInput = useRef()
+  const context = useContext(MyContext) 
+  const [error,setError]=useState([false,''])
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    const value=textInput.current.value
+    if (validation(value)){
+      setError([false,""])
+      context.addPlayer(value)
+      textInput.current.value=""
+    }else{
+      console.log("error")
+    }
+  }
+  const validation=(value)=>{
+    if(value===""){
+      setError([true,"You must type"])
+      return false
+    }
+    if (value.length <=2){
+      setError([true,"You must enter bigger"])
+      return false
+    }
+    return true
+  }
+  console.log(context)
+  return <div>
+    <Form className="mb-4" onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Control
+          type="text"
+          placeholder="Add player"
+          name="player"
+          ref={textInput}
+        />
+      </Form.Group>
+      {
+        error[0] ?
+          <Alert variant="danger">
+            {error[1]}
+          </Alert>
+        :null
+      }
+      <Button className="mb-4" variant="primary" type="submit">
+        App player
+      </Button>
+
+    </Form>
+  </div>;
 }
 
 export default Stage1;
--- src/context/index.js
+++ src/context/index.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useRef, useContext, useState } from "react";
+import { Button } from "react-bootstrap"
 
-const MyContext=React.createContext();
+const MyContext = React.createContext();
 
-class MyProvider extends React.Component{
-    state={
-        stage:1,
-        players:[],
-        result:''
+class MyProvider extends React.Component {
+    state = {
+        stage: 1,
+        players: [],
+        result: ''
     }
 
-    render(){
-        return(
-            <MyContext.Provider value={{state:this.state}}>
+    addPlayerHandler = (name) => {
+        this.setState((prevState) => ({
+            players: [
+                ...prevState.players,
+                name
+            ]
+        }))
+    }
+
+    render() {
+        return (
+            <MyContext.Provider
+                value={{
+                    state: this.state,
+                    addPlayer: this.addPlayerHandler
+                }}
+
+            >
                 {this.props.children}
             </MyContext.Provider>
         )
     }
 }
 
-export {MyContext,MyProvider}
+export { MyContext, MyProvider }
